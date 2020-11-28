377308b0997f487d83b0038bcf7f1e7cb61f5a45
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2020-11-28 01:01:58 +0100

    ArmComponent: Add FAKE_ARM mode

--- src/Arm_component/ArmComponent.cpp
+++ src/Arm_component/ArmComponent.cpp
@@ -14,13 +14,28 @@
 #include <yarp/os/Bottle.h>
 #include <yarp/os/RpcClient.h>
 #include <yarp/os/Network.h>
+#include <yarp/os/LogComponent.h>
 
 #include <thread>
 #include <chrono>
 #include <iostream>
+#include <mutex>
+
 using namespace yarp::dev;
 using namespace yarp::os;
 using namespace std;
+using namespace std::chrono_literals;
+
+namespace {
+YARP_LOG_COMPONENT(ARMCOMPONENT,
+                   "scope.component.Arm",
+                   yarp::os::Log::minimumPrintLevel(),
+                   yarp::os::Log::LogTypeReserved,
+                   yarp::os::Log::printCallback(),
+                   nullptr)
+}
+
+#define FAKE_ARM
 
 class ArmComponent : public ArmService
 {
@@ -31,7 +46,7 @@ public:
     {
         Network yarp;
 
-        RpcClient port;
+#if !defined(FAKE_ARM)
         m_client_port_arm.open(m_client_name_arm);
         m_client_port_wrist.open(m_client_name_wrist);
         m_client_port_hand.open(m_client_name_hand);
@@ -43,6 +58,7 @@ public:
         yarp.connect(m_client_name_arm, m_server_name_arm);
         yarp.connect(m_client_name_wrist, m_server_name_wrist);
         yarp.connect(m_client_name_hand, m_server_name_hand);
+#endif
 
         this->yarp().attachAsServer(server_port);
         if (!server_port.open("/ArmComponent"))
@@ -61,8 +77,9 @@ public:
 
     bool preGrasp() override
     {
-        yInfo("preGrasp");
+        yCInfo(ARMCOMPONENT, "preGrasp");
 
+#if !defined(FAKE_ARM)
         Bottle cmd, response;
         cmd.addString("set");
         cmd.addString("pos");
@@ -95,15 +112,17 @@ public:
 
             m_client_port_arm.write(cmd,response);
             real_value = response.get(2).asDouble();
-            //yInfo() << "Waiting shuoulder";
+            //yCInfo(ARMCOMPONENT) << "Waiting shuoulder";
 
         }
+#endif
         return true;
     }
 
     bool extractHand() override
     {
-        yInfo("extractHand");
+        yCInfo(ARMCOMPONENT, "extractHand");
+#if !defined(FAKE_ARM)
         Bottle cmd, response;
 
         cmd.addString("set");
@@ -138,14 +157,20 @@ public:
             m_client_port_wrist.write(cmd,response);
             real_value = response.get(2).asDouble();
         }
+#else
+        std::lock_guard<std::mutex> lock(mutex);
+        std::this_thread::sleep_for(4s);
+        extracted = true;
+#endif
 
         return true;
     }
 
     bool retractHand() override
     {
-        yInfo("retractHand");
+        yCInfo(ARMCOMPONENT, "retractHand");
 
+#if !defined(FAKE_ARM)
         Bottle cmd, response;
         cmd.clear();
         response.clear();
@@ -180,13 +205,20 @@ public:
             m_client_port_wrist.write(cmd,response);
             real_value = response.get(2).asDouble();
         }
+#else
+        std::lock_guard<std::mutex> lock(mutex);
+        std::this_thread::sleep_for(4s);
+        extracted = false;
+#endif
+
         return true;
     }
 
     bool closeHand() override
     {
-        yInfo("closeHand");
+        yCInfo(ARMCOMPONENT, "closeHand");
 
+#if !defined(FAKE_ARM)
         Bottle cmd, response;
         cmd.addString("set");
         cmd.addString("pos");
@@ -207,13 +239,22 @@ public:
 
         std::this_thread::sleep_for(std::chrono::milliseconds(2000));
 
+#else
+        std::lock_guard<std::mutex> lock(mutex);
+        std::this_thread::sleep_for(2s);
+        if (extracted && opened) {
+            grasped = true;
+        }
+        opened = false;
+#endif
         return true;
     }
 
     bool openHand() override
     {
-        yInfo("openHand");
+        yCInfo(ARMCOMPONENT, "openHand");
 
+#if !defined(FAKE_ARM)
         Bottle cmd, response;
         cmd.addString("set");
         cmd.addString("pos");
@@ -233,13 +274,21 @@ public:
         m_client_port_hand.write(cmd,response);
 
         std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+#else
+        std::lock_guard<std::mutex> lock(mutex);
+        std::this_thread::sleep_for(2s);
+        opened = true;
+        grasped = false;
+#endif
 
         return true;
     }
 
     bool hasGrasped() override
     {
-      yInfo("hasGrasped");
+      yCInfo(ARMCOMPONENT, "hasGrasped");
+
+#if !defined(FAKE_ARM)
       // checks 3 times
       bool has_grasped = false;
       unsigned int i = 0;
@@ -267,64 +316,73 @@ public:
         has_grasped = ( value_0 > 40 && value_0 < 75);
         if (has_grasped)
         {
-          yInfo("something in the hand, checking again");
-          std::this_thread::sleep_for(std::chrono::milliseconds(200));
+            yCInfo(ARMCOMPONENT, "something in the hand, checking again");
+            std::this_thread::sleep_for(std::chrono::milliseconds(200));
         }
         else
         {
-          yInfo("nothing in the hand");
-          return false;
+            yCInfo(ARMCOMPONENT, "nothing in the hand");
+            return false;
 
         }
       }
 
         return has_grasped;
+#else
+        std::lock_guard<std::mutex> lock(mutex);
+        return grasped;
+#endif
 
     }
 
     bool home() override
     {
-      retractHand();
-      closeHand();
+        yCInfo(ARMCOMPONENT, "home");
+        retractHand();
+        closeHand();
 
-      // move arm home
-      Bottle cmd, response;
-      cmd.addString("set");
-      cmd.addString("pos");
-      cmd.addInt32(0);
-      cmd.addDouble(0);
+#if !defined(FAKE_ARM)
+        // move arm home
+        Bottle cmd, response;
+        cmd.addString("set");
+        cmd.addString("pos");
+        cmd.addInt32(0);
+        cmd.addDouble(0);
 
-      m_client_port_arm.write(cmd,response);
+        m_client_port_arm.write(cmd,response);
 
-      // checking with in actually gets there
-      cmd.clear();
-      response.clear();
+        // checking with in actually gets there
+        cmd.clear();
+        response.clear();
 
-      cmd.addString("get");
-      cmd.addString("enc");
-      cmd.addInt32(0);
+        cmd.addString("get");
+        cmd.addString("enc");
+        cmd.addInt32(0);
 
-      m_client_port_arm.write(cmd,response);
+        m_client_port_arm.write(cmd,response);
 
-      double real_value = response.get(2).asDouble();
+        double real_value = response.get(2).asDouble();
 
-      while (abs(real_value) > 2)
-      {
-          std::this_thread::sleep_for(std::chrono::milliseconds(100));
-          cmd.clear();
-          response.clear();
+        while (abs(real_value) > 2)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            cmd.clear();
+            response.clear();
 
-          cmd.addString("get");
-          cmd.addString("enc");
-          cmd.addInt32(0);
+            cmd.addString("get");
+            cmd.addString("enc");
+            cmd.addInt32(0);
 
-          m_client_port_arm.write(cmd,response);
-          real_value = response.get(2).asDouble();
-          //yInfo() << "Waiting shuoulder";
+            m_client_port_arm.write(cmd,response);
+            real_value = response.get(2).asDouble();
+            //yInfo() << "Waiting shoulder";
 
-      }
+        }
+#else
+        std::lock_guard<std::mutex> lock(mutex);
+        std::this_thread::sleep_for(5s);
+#endif
 
-        yInfo("home");
         return true;
     }
 
@@ -334,6 +392,7 @@ private:
 
     yarp::os::RpcServer server_port;
 
+#if !defined(FAKE_ARM)
     string m_client_name_arm = "/ArmComponent/arm/rpc:o";
     string m_client_name_wrist = "/ArmComponent/wrist/rpc:o";
     string m_client_name_hand = "/ArmComponent/hand/rpc:o";
@@ -345,11 +404,16 @@ private:
     RpcClient m_client_port_arm;
     RpcClient m_client_port_wrist;
     RpcClient m_client_port_hand;
-
     double m_arm_target = 40;
     double m_wrist_target = 0.1;
     double m_hand_closed_target = 80;
     double m_hand_open_target = 20;
+#else
+    std::mutex mutex;
+    bool extracted {false};
+    bool opened {false};
+    bool grasped {false};
+#endif
 
 };
 
@@ -363,7 +427,7 @@ int main()
     }
 
     while (true) {
-        yInfo("Server running happily");
+        yCInfo(ARMCOMPONENT, "Server running happily");
         yarp::os::Time::delay(10);
     }
 
